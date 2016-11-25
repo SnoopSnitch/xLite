1444c96123db379566b6c96d0e0839748dc23991
Emi <emanuel@srlabs.de>
2016-11-25 12:14:08 +0200

    Updated tasks in top-level build.gradle + libcheck.sh placeholder.

--- SnoopSnitch/do_nothing2.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash -e
-echo "(2) Here we run compile script! But I'm not it..."
-
---
+++ SnoopSnitch/libcheck.sh
@@ -0,0 +1,7 @@
+#! /bin/bash -e
+# libcheck.sh -- Checks if all the required git submodules are present
+echo "(2) libcheck.sh placeholder script."
+echo "    this script should check if the submodules present..."
+echo "WIP!"
+
+exit 0
