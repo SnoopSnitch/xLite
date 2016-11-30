04eda84ad13fd7404fab85a9d2bfdac731ec8767
Emi <emanuel@srlabs.de>
2016-11-30 11:38:42 +0200

    - Added v4 compat to build.gradle - Failed attempt to resolve unconditional layout inflation - Commented out superflous do-nothing code - Tried to resolve getColor deprication warnings, using:   ContextCompat.getColor(context, <color>)

--- SnoopSnitch/app/src/main/java/de/srlabs/snoopsnitch/views/adapter/ListViewEventAdapter.java
+++ SnoopSnitch/app/src/main/java/de/srlabs/snoopsnitch/views/adapter/ListViewEventAdapter.java
@@ -15,22 +15,22 @@ import android.widget.Button;
 import android.widget.Filter;
 import android.widget.Filterable;
 import android.widget.TextView;
+import android.support.v4.content.ContextCompat;
+
 import de.srlabs.snoopsnitch.R;
 import de.srlabs.snoopsnitch.DetailChartActivity;
 import de.srlabs.snoopsnitch.analysis.Event;
 import de.srlabs.snoopsnitch.analysis.Event.Type;
 import de.srlabs.snoopsnitch.util.MsdDialog;
 
-public class ListViewEventAdapter extends ArrayAdapter<Event> implements Filterable
-{
+public class ListViewEventAdapter extends ArrayAdapter<Event> implements Filterable {
 	// Attributes
 	private final Context context;
 	private final Vector<Event> allEvents;
 	private Vector<Event> values;
 	private DetailChartActivity host;
 	
-	public ListViewEventAdapter (Context context, Vector<Event> values) 
-	{
+	public ListViewEventAdapter (Context context, Vector<Event> values) {
 	    super(context, R.layout.custom_row_layout_sms, values);
 	    this.context = context;
 	    this.allEvents = values;
@@ -39,22 +39,22 @@ public class ListViewEventAdapter extends ArrayAdapter<Event> implements Filtera
 	}
 	
 	@Override
-	public View getView(final int position, View convertView, ViewGroup parent) 
-	{
-		LayoutInflater inflater = (LayoutInflater) context
-			.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		final View rowView = inflater.inflate(R.layout.custom_row_layout_sms, parent, false);
-		
+	public View getView(final int position, View convertView, ViewGroup parent) {
+		// We should probably use a ViewHolder:
+		// http://stackoverflow.com/questions/25381435/unconditional-layout-inflation-from-view-adapter-should-use-view-holder-patter
+		//View rowView = convertView;
+		//if (rowView == null) {
+			LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			final View rowView = inflater.inflate(R.layout.custom_row_layout_sms, parent, false);
+			//rowView = inflater.inflate(R.layout.custom_row_layout_sms, parent, false);
+		//}
+
 		// Set type
-		if (values.get(position).getType().name().equals(Type.BINARY_SMS.name()))
-		{
+		if (values.get(position).getType().name().equals(Type.BINARY_SMS.name())) {
 			((TextView) rowView.findViewById(R.id.txtSmsRowTypeValue)).setText(context.getResources().getString(R.string.common_binary_sms));			
-		} else if (values.get(position).getType().name().equals(Type.SILENT_SMS.name()))
-		{
+		} else if (values.get(position).getType().name().equals(Type.SILENT_SMS.name())) {
 			((TextView) rowView.findViewById(R.id.txtSmsRowTypeValue)).setText(context.getResources().getString(R.string.common_silent_sms));			
-		}
-		else if (values.get(position).getType().name().equals(Type.NULL_PAGING.name()))
-		{
+		} else if (values.get(position).getType().name().equals(Type.NULL_PAGING.name()))	{
 			((TextView) rowView.findViewById(R.id.txtSmsRowTypeValue)).setText(context.getResources().getString(R.string.common_null_paging));					
 		}
 		
@@ -78,106 +78,103 @@ public class ListViewEventAdapter extends ArrayAdapter<Event> implements Filtera
 		// Check upload state and set button
 		final Button btnUpload = (Button) rowView.findViewById(R.id.btnUploadSms);
 		
-		switch (values.get(position).getUploadState()) 
-		{
-		case STATE_UPLOADED:
-			btnUpload.setBackgroundResource(R.drawable.ic_content_checkmark);
-			btnUpload.setText("");
-			btnUpload.setEnabled(false);
-			btnUpload.setVisibility(View.VISIBLE);
-			rowView.setBackgroundColor(context.getResources().getColor(R.color.common_custom_row_background_disabled));
-			break;
-		case STATE_AVAILABLE:
-			btnUpload.setBackgroundResource(R.drawable.bt_content_contributedata_enable);
-			btnUpload.setText(context.getResources().getString(R.string.common_button_upload));
-			btnUpload.setEnabled(true);
-			btnUpload.setVisibility(View.VISIBLE);
-			btnUpload.setOnClickListener(new View.OnClickListener() 
-			{		
-				@Override
-				public void onClick(View v) 
+		switch (values.get(position).getUploadState()) {
+			case STATE_UPLOADED:
+				btnUpload.setBackgroundResource(R.drawable.ic_content_checkmark);
+				btnUpload.setText("");
+				btnUpload.setEnabled(false);
+				btnUpload.setVisibility(View.VISIBLE);
+				//rowView.setBackgroundColor(context.getResources().getColor(R.color.common_custom_row_background_disabled));
+				rowView.setBackgroundColor(ContextCompat.getColor(context, R.color.common_custom_row_background_disabled));
+				break;
+			case STATE_AVAILABLE:
+				btnUpload.setBackgroundResource(R.drawable.bt_content_contributedata_enable);
+				btnUpload.setText(context.getResources().getString(R.string.common_button_upload));
+				btnUpload.setEnabled(true);
+				btnUpload.setVisibility(View.VISIBLE);
+				btnUpload.setOnClickListener(new View.OnClickListener()
 				{
-					MsdDialog.makeConfirmationDialog(host, host.getResources().getString(R.string.alert_upload_message), 
-							new OnClickListener() 
-					{			
-						@Override
-						public void onClick(DialogInterface dialog, int which) 
+					@Override
+					public void onClick(View v)
+					{
+						MsdDialog.makeConfirmationDialog(host, host.getResources().getString(R.string.alert_upload_message),
+								new OnClickListener()
 						{
-							values.get(position).upload();
-							btnUpload.setBackgroundResource(R.drawable.ic_content_checkmark);
-							btnUpload.setText("");
-							btnUpload.setEnabled(false);
-							btnUpload.setVisibility(View.VISIBLE);
-							rowView.setBackgroundColor(context.getResources().getColor(R.color.common_custom_row_background_disabled));
-							host.refreshView();
-						}
-					}, null, false).show();
-				}
-			});
-			break;
-		case STATE_DELETED:
-			btnUpload.setBackgroundResource(R.drawable.bt_content_contributedata_disable);
-			btnUpload.setText(context.getResources().getString(R.string.common_button_nodata));
-			btnUpload.setEnabled(false);
-			btnUpload.setVisibility(View.VISIBLE);
-			rowView.setBackgroundColor(context.getResources().getColor(R.color.common_custom_row_background_disabled));
-			break;
-		case STATE_INVALID:
-			btnUpload.setBackgroundResource(R.drawable.bt_content_contributedata_disable);
-			btnUpload.setText(context.getResources().getString(R.string.common_button_nodata));
-			btnUpload.setEnabled(false);
-			btnUpload.setVisibility(View.VISIBLE);
-			rowView.setBackgroundColor(context.getResources().getColor(R.color.common_custom_row_background_disabled));
-			break;
-		default:
-			btnUpload.setEnabled(false);
-			btnUpload.setVisibility(View.GONE);
-			break;
+							@Override
+							public void onClick(DialogInterface dialog, int which)
+							{
+								values.get(position).upload();
+								btnUpload.setBackgroundResource(R.drawable.ic_content_checkmark);
+								btnUpload.setText("");
+								btnUpload.setEnabled(false);
+								btnUpload.setVisibility(View.VISIBLE);
+								//rowView.setBackgroundColor(context.getResources().getColor(R.color.common_custom_row_background_disabled));
+								rowView.setBackgroundColor(ContextCompat.getColor(context, R.color.common_custom_row_background_disabled));
+								host.refreshView();
+							}
+						}, null, false).show();
+					}
+				});
+				break;
+			case STATE_DELETED:
+				btnUpload.setBackgroundResource(R.drawable.bt_content_contributedata_disable);
+				btnUpload.setText(context.getResources().getString(R.string.common_button_nodata));
+				btnUpload.setEnabled(false);
+				btnUpload.setVisibility(View.VISIBLE);
+				//rowView.setBackgroundColor(context.getResources().getColor(R.color.common_custom_row_background_disabled));
+				rowView.setBackgroundColor(ContextCompat.getColor(context, R.color.common_custom_row_background_disabled));
+				break;
+			case STATE_INVALID:
+				btnUpload.setBackgroundResource(R.drawable.bt_content_contributedata_disable);
+				btnUpload.setText(context.getResources().getString(R.string.common_button_nodata));
+				btnUpload.setEnabled(false);
+				btnUpload.setVisibility(View.VISIBLE);
+				//rowView.setBackgroundColor(context.getResources().getColor(R.color.common_custom_row_background_disabled));
+				rowView.setBackgroundColor(ContextCompat.getColor(context, R.color.common_custom_row_background_disabled));
+				break;
+			default:
+				btnUpload.setEnabled(false);
+				btnUpload.setVisibility(View.GONE);
+				break;
 		}
 		
 		return rowView;
 	 }
 	
 	@Override
-	public Filter getFilter() 
-	{
-		return new Filter() 
-		{		
+	public Filter getFilter() {
+		return new Filter() {
 			@Override
 			protected void publishResults(CharSequence constraint, FilterResults results) {
 				// ToDo: Fix this! What is it supposed to do?
 				// In addition, this gives an [unchecked] cast warning.
-				if (results.count == 0) {
+				//if (results.count == 0) {
 					//notifyDataSetInvalidated();
 					values = (Vector<Event>) results.values;
 					notifyDataSetChanged();
-				} else {
-					values = (Vector<Event>) results.values;
-					notifyDataSetChanged();
-				}
+				//} else {
+				//	values = (Vector<Event>) results.values;
+				//	notifyDataSetChanged();
+				//}
 			}
 		
 			@Override
-			protected FilterResults performFiltering(CharSequence smsType) 
-			{
+			protected FilterResults performFiltering(CharSequence smsType) {
 				FilterResults results = new FilterResults();
-				Vector<Event> eventList = new Vector<Event>();
+				Vector<Event> eventList = new Vector<>();
 				
-				if (smsType.equals("ALL"))
-				{
+				if (smsType.equals("ALL")) {
 					results.values = allEvents;
 					results.count = allEvents.size();
 					return results;
 				}
-				
-				for (Event event : allEvents) 
-				{
-					if (event.getType().toString().equals(smsType))
-					{
+
+				for (Event event : allEvents) {
+					if (event.getType().toString().equals(smsType))	{
 						eventList.add(event);
 					}
 				}
-				
+
 				results.values = eventList;
 				results.count = eventList.size();
 				
